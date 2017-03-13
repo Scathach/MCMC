7df08a75293d5809e3b10024c9884247fadd5c0b
Misha Savchenko <savchenkmisha@gmail.com>
2017-03-12 23:23:21 -0400

    variable red

--- line0000.py
+++ line0000.py
@@ -102,39 +102,35 @@ sampler.run_mcmc(pos, 500, rstate0=np.random.get_state())
 print("Done.")
 
 pl.clf()
-fig, axes = pl.subplots(3, 1, sharex=True, figsize=(8, 9))
+fig, axes = pl.subplots(2, 1, sharex=True, figsize=(8, 9))
 axes[0].plot(sampler.chain[:, :, 0].T, color="k", alpha=0.4)
 axes[0].yaxis.set_major_locator(MaxNLocator(5))
-axes[0].axhline(m_true, color="#888888", lw=2)
-axes[0].set_ylabel("$m$")
+#axes[0].axhline(m_true, color="#888888", lw=2)
+axes[0].set_ylabel("$T$")
 
-axes[1].plot(sampler.chain[:, :, 1].T, color="k", alpha=0.4)
+axes[1].plot(np.exp(sampler.chain[:, :, 2]).T, color="k", alpha=0.4)
 axes[1].yaxis.set_major_locator(MaxNLocator(5))
-axes[1].axhline(b_true, color="#888888", lw=2)
-axes[1].set_ylabel("$b$")
-
-axes[2].plot(np.exp(sampler.chain[:, :, 2]).T, color="k", alpha=0.4)
-axes[2].yaxis.set_major_locator(MaxNLocator(5))
-axes[2].axhline(f_true, color="#888888", lw=2)
-axes[2].set_ylabel("$f$")
-axes[2].set_xlabel("step number")
+#axes[2].axhline(f_true, color="#888888", lw=2)
+axes[1].set_ylabel("$f$")
+axes[1].set_xlabel("step number")
 
 fig.tight_layout(h_pad=0.0)
 fig.savefig("line-time.png")
 
 # Make the triangle plot.
 burnin = 50
-samples = sampler.chain[:, burnin:, :].reshape((-1, ndim))
-
+samples = sampler.chain[:,burnin:, :].reshape((-1, 2))
+print(samples,"samples")
+"""
 fig = corner.corner(samples, labels=["$m$", "$b$", "$\ln\,f$"],
                       truths=[m_true, b_true, np.log(f_true)])
 fig.savefig("line-triangle.png")
-
+"""
 # Plot some samples onto the data.
 pl.figure()
-for a, T_ls, lnf in samples[np.random.randint(len(samples), size=100)]:
-    pl.plot(xl, (a/xl**5)*(1/((np.exp(b/(xl*T_ls[0]))-1))), color="k", alpha=0.1)
-pl.plot(xl, m_true*xl+b_true, color="r", lw=2, alpha=0.8)
+for T_ls, lnf in samples[np.random.randint(len(samples), size=100)]:
+    pl.plot(xl, (a/xl**5)*(1/((np.exp(b/(xl*T_ls))-1))), color="k", alpha=0.1)
+#pl.plot(xl, m_true*xl+b_true, color="r", lw=2, alpha=0.8)
 pl.errorbar(x, y, yerr=yerr, fmt=".k")
 pl.xlabel("$x$")
 pl.ylabel("$y$")
@@ -142,12 +138,12 @@ pl.tight_layout()
 pl.savefig("line-mcmc.png")
 
 # Compute the quantiles.
-samples[:, 2] = np.exp(samples[:, 2])
-m_mcmc, b_mcmc, f_mcmc = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
+samples[:, 1] = np.exp(samples[:, 1])
+T_mcmc, f_mcmc = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]),
                              zip(*np.percentile(samples, [16, 50, 84],
                                                 axis=0)))
+print(T_mcmc)
 print("""MCMC result:
-    m = {0[0]} +{0[1]} -{0[2]} (truth: {1})
-    b = {2[0]} +{2[1]} -{2[2]} (truth: {3})
-    f = {4[0]} +{4[1]} -{4[2]} (truth: {5})
-""".format(m_mcmc, m_true, b_mcmc, b_true, f_mcmc, f_true))
+    T = {0[0]} +{0[1]} -{0[2]}
+    f = {1[0]} +{1[1]} -{1[2]}
+""".format(T_mcmc, f_mcmc))

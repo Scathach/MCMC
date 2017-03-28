767374e86273e7d52bdd11571dff5c10d14c920a
Misha Savchenko <savchenkmisha@gmail.com>
2017-03-27 21:41:28 -0400

    minor cleanup prefomed

--- mcmc.py
+++ mcmc.py
@@ -1,8 +1,4 @@
 
-# coding: utf-8
-
-# In[2]:
-
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy.io import ascii
@@ -21,9 +17,6 @@ logfacmin = -100.0 #log factor minimum
 logfacmax = 0.0 #log factor maximum
 thetashape=np.array([[Teffmin,Teffmax],[logfacmin,logfacmax]])
 
-
-# In[3]:
-
 # Set a definition for the model
 def model(microns,Teff,logfactor):
   wavelength = microns*1.0e-6
@@ -36,9 +29,6 @@ def model(microns,Teff,logfactor):
     logflux[i] = logfactor + np.log10(flux[i])
   return logflux
 
-
-# In[4]:
-
 # Set a definition for the loglikelihood, assuming normally distributed data
 def log_like(lam,logf,errlogf,theta):
     residuals = logf - model(lam,theta[0],theta[1])
@@ -47,9 +37,6 @@ def log_like(lam,logf,errlogf,theta):
         loglike = loglike - np.log(errlogf[i]) - 0.5*(residuals[i]/errlogf[i])**2
     loglike = loglike - 0.5*len(lam)*np.log(2.0*np.pi)
     return loglike
-
-
-# In[5]:
 
 # Set a definition for the logpriors
 def log_prior(theta,thetashape):
@@ -76,10 +63,8 @@ def log_prior(theta,thetashape):
         logpriors[1] = -1.0e99 # -infinity
 
     #logprior = np.sum(logpriors)
+
     return np.sum(logpriors)
-
-
-# In[6]:
 
 # Initialize the MCMC from a random point drawn from the prior
 Teffinitial = np.exp( np.random.uniform(np.log(thetashape[0][0]),np.log(thetashape[0][1])) )
@@ -89,9 +74,6 @@ print(Teffinitial)
 # Calculate the associated modified loglike
 loglikechain=np.empty([1])
 loglikechain[0]=log_prior(thetachain[0],thetashape) + log_like(lam,logf,errlogf,thetachain[0])
-
-
-# In[7]:
 
 # Define the proposal jump size
 Teffjump=5
@@ -124,9 +106,6 @@ while True:
         break
 
 
-# In[52]:
-
-
 jlist=np.arange(len(thetachain))
 plt.scatter(thetachain[:,0], thetachain[:,1], c=jlist, cmap='coolwarm')
 plt.xlabel('Temperature [K]')
@@ -134,21 +113,13 @@ plt.ylabel('log10(factor)')
 plt.show()
 
 
-# In[53]:
-
 np.max(loglikechain)
-
-
-# In[54]:
 
 
 plt.plot(loglikechain)
 plt.xlabel('Chain number')
 plt.ylabel('loglike')
 plt.show()
-
-
-# In[13]:
 
 loglikeburn=np.median(loglikechain)
 j=-1
@@ -160,9 +131,6 @@ burnj=j
 print( 'Burn point = ',burnj)
 
 
-# In[14]:
-
-
 jlist=np.arange(len(thetachain))
 plt.scatter(thetachain[burnj:,0], thetachain[burnj:,1], c=jlist[burnj:], cmap='coolwarm',alpha=0.5)
 plt.xlabel('Temperature [K]')
@@ -171,13 +139,7 @@ plt.show()
 print( 'Temperature [K] = ',np.round(np.median(thetachain[burnj:,0]),1),'-',np.round(np.median(thetachain[burnj:,0])-np.percentile(thetachain[burnj:,0],15.9),1),'+',np.round(np.percentile(thetachain[burnj:,0],84.1)-np.median(thetachain[burnj:,0]),1))
 print( 'log10(factor) = ',np.round(np.median(thetachain[burnj:,1]),3),'-',np.round(np.median(thetachain[burnj:,1])-np.percentile(thetachain[burnj:,1],15.9),3),'+',np.round(np.percentile(thetachain[burnj:,1],84.1)-np.median(thetachain[burnj:,1]),3))
 
-
-# In[51]:
-
 ascii.write(thetachain[burnj:,:], "chains.dat")
-
-
-# In[16]:
 
 
 plt.plot(thetachain[burnj:,0])
@@ -187,17 +149,11 @@ plt.ylabel('Temperature [K]')
 plt.show()
 
 
-# In[17]:
-
-
 plt.plot(thetachain[burnj:,1])
 plt.title('Check mixing')
 plt.xlabel('Chain number')
 plt.ylabel('log10(factor)')
 plt.show()
-
-
-# In[49]:
 
 
 temp=np.empty([len(thetachain)-burnj])
@@ -210,10 +166,6 @@ plt.xlabel('Chain number')
 plt.ylabel('Temperature [K]')
 plt.show()
 
-
-# In[50]:
-
-
 temp=np.empty([len(thetachain)-burnj])
 temp[0]=thetachain[burnj,1]
 for i in range(burnj+1,len(thetachain)):
@@ -223,6 +175,3 @@ plt.title('Check mixing')
 plt.xlabel('Chain number')
 plt.ylabel('log10(factor)')
 plt.show()
-
-
-# In[ ]:
